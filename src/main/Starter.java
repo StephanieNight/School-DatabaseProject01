caf4baaebcab7bc4ae11129fa37a24aa4d52cd69
StephanieNigth <s.hvenegaard@outlook.com>
2017-11-22 11:30:28 +0100

    base classes

--- src/main/Starter.java
+++ src/main/Starter.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ihopethensaisnotwaching;
+package main;
 
 /**
  *
  * @author Stephanie
  */
-public class IhopeTheNSAIsNotWaching {
+public class Starter {
 
     /**
      * @param args the command line arguments
